cc3df3993e01102e3974c601e15c8b1843500f66
Ross Condie <rosscondie@gmail.com>
2023-04-06 10:29:17 +0200

    added icons for the floating nav bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import './Nav.css';
 import { IoHome } from 'react-icons/io5';
+import { FaUserAlt } from 'react-icons/fa';
+import { ImBook } from 'react-icons/im';
+import { FaBriefcase } from 'react-icons/fa';
+import { MdMessage } from 'react-icons/md';
 
 const Nav = () => {
   return (
     <nav>
       <a href="#">
         <IoHome />
+      </a>
+      <a href="#about">
+        <FaUserAlt />
+      </a>
+      <a href="#experience">
+        <ImBook />
+      </a>
+      <a href="#portfolio">
+        <FaBriefcase />
+      </a>
+      <a href="#contact">
+        <MdMessage />
       </a>
     </nav>
   );
