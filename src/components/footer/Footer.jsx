6a5e0863084c16dc92e2be9c482cf82e6489486e
Ross Condie <rosscondie@gmail.com>
2023-04-08 13:41:05 +0200

    finished with basic setup - need to refactor

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,56 @@
 import React from 'react';
 import './Footer.css';
+import { BsGithub } from 'react-icons/bs';
+import { BsLinkedin } from 'react-icons/bs';
+import { BsInstagram } from 'react-icons/bs';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        RA CONDIE
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About </a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://github.com">
+          <BsGithub />
+        </a>
+        <a href="https://linkedin.com">
+          <BsLinkedin />
+        </a>
+        <a href="https://instagram.com">
+          <BsInstagram />
+        </a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; Ross Condie. All Rights Reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
