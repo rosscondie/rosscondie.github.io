f7f7484451564c44ddbed79ac3386d9847836e46
Ross Condie <rosscondie@gmail.com>
2023-04-08 12:06:33 +0200

    added testimonial section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,62 @@
 import React from 'react';
 import './Portfolio.css';
+import IMG1 from '../../assets/portfolio1.jpg';
+import IMG2 from '../../assets/portfolio2.jpg';
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Fugitive Travel',
+    github: 'https://github.com/rosscondie/Python_Flask_Project',
+    demo: '',
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Atlas Wealth',
+    github: 'https://github.com/rosscondie/Atlas_Wealth_Group_Project',
+    demo: '',
+  },
+];
 
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a
+                    href={github}
+                    className="btn"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Github
+                  </a>
+                  <a
+                    href={demo}
+                    className="btn btn-primary"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Live Demo
+                  </a>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,53 @@
 import React from 'react';
 import './Testimonials.css';
+import AVTR1 from '../../assets/avatar1.jpg';
+import AVTR2 from '../../assets/avatar2.jpg';
+import AVTR3 from '../../assets/avatar3.jpg';
+
+const data = [
+  {
+    id: 1,
+    avatar: AVTR1,
+    name: 'Tina Snow',
+    review:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam possimus fugit veritatis dicta ad accusantium? Ipsum ea impedit hic ratione!',
+  },
+  {
+    id: 2,
+    avatar: AVTR2,
+    name: 'Joe Bloggs',
+    review:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam possimus fugit veritatis dicta ad accusantium? Ipsum ea impedit hic ratione!',
+  },
+  {
+    id: 3,
+    avatar: AVTR3,
+    name: 'Bloggy Joe',
+    review:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam possimus fugit veritatis dicta ad accusantium? Ipsum ea impedit hic ratione!',
+  },
+];
 
 const Testimonials = () => {
-  return <section id="testimonials">Testimonials</section>;
+  return (
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+      <div className="container testimonials__container">
+        {data.map(({ id, avatar, name, review }) => {
+          return (
+            <article key={id} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt="Avatar One" />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
