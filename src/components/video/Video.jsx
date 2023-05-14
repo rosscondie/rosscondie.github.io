bfe22e834f11b48de7ce06f70dfb29a74607e522
Ross Condie <rosscondie@gmail.com>
2023-05-14 13:20:48 +0200

    added flex and align-items: center to portfolio container

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -4,7 +4,7 @@ export const Video = () => {
   return (
     <div className="video-container">
       <iframe
-        width="400"
+        width="385"
         height="200"
         src="https://www.youtube.com/embed/591Hzn0-AJU"
         title="YouTube video player"
