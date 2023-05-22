4b54619932011ed4b64b2c674a1774aeab82eec6
Ross Condie <rosscondie@gmail.com>
2023-05-22 16:03:13 +0200

    working on media queries

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Video.css';
 
 export const Video = () => {
   return (
     <div className="video-container">
       <iframe
-        width="385"
+        width="370"
         height="200"
         src="https://www.youtube.com/embed/591Hzn0-AJU"
         title="YouTube video player"
