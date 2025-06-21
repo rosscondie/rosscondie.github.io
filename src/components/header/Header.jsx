6639128ddeef1b2d3f743b2ac62c6312f6900adb
Ross Condie <rosscondie@gmail.com>
2025-06-21 20:19:27 +0200

    Migrate from CRA to Vite

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,35 +1,34 @@
 import React from "react";
-import CTA from "./CTA";
 import "./Header.css";
 import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
-  return (
-    <header>
-      <div className="container header__container">
-        <div className="dev-symbol">&lt;/&gt;</div>
-        <div className="terminal-window">
-          <div className="terminal-prompt">
-            <span className="prompt-user">user</span>
-            <span className="prompt-separator">@</span>
-            <span className="prompt-host">portfolio</span>
-            <span className="prompt-colon">:</span>
-            <span className="prompt-path">~</span>
-            <span className="prompt-symbol">$&nbsp;</span>
-            <span className="command typing-animation">go run hello.go</span>
-          </div>
-          <div className="terminal-output">
-            <span className="output-text delayed-output">
-              <span className="go-string">👋 Hello I'm Ross</span>
-            </span>
-          </div>
-        </div>
-        <HeaderSocials />
-        <span>Not a 10x developer.</span>
-        <span>Just someone who reads error messages and Googles well.</span>
-      </div>
-    </header>
-  );
+	return (
+		<header>
+			<div className="container header__container">
+				<div className="dev-symbol">&lt;/&gt;</div>
+				<div className="terminal-window">
+					<div className="terminal-prompt">
+						<span className="prompt-user">user</span>
+						<span className="prompt-separator">@</span>
+						<span className="prompt-host">portfolio</span>
+						<span className="prompt-colon">:</span>
+						<span className="prompt-path">~</span>
+						<span className="prompt-symbol">$&nbsp;</span>
+						<span className="command typing-animation">go run hello.go</span>
+					</div>
+					<div className="terminal-output">
+						<span className="output-text delayed-output">
+							<span className="go-string">👋 Hello I'm Ross</span>
+						</span>
+					</div>
+				</div>
+				<HeaderSocials />
+				<span>Not a 10x developer.</span>
+				<span>Just someone who reads error messages and Googles well.</span>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-const rootElement = document.querySelector('#root');
-ReactDOM.createRoot(rootElement).render(<App />);
