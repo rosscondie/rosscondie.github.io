261c332d287a57a1fe947dc82e5c89c7e3930305
Ross Condie <rosscondie@gmail.com>
2025-06-22 11:57:57 +0200

    Test deployment on push

--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -3,29 +3,32 @@ import "./Description.css";
 import CCSYLLABUS from "../../assets/code_clan_syllabus_psd.pdf";
 
 const Description = () => {
-  return (
-    <section className="description__section">
-      <div className="container description__container">
-        <h3>
-          Software Developer @ <a href="https://www.vier.ai/">VIER GmbH</a>
-          <br />
-        </h3>
-        <br />
-        <h3>
-          Currently diving deep into AI tools and their practical applications
-          in software development.
-          <br />
-          <br />
-          When I’m not coding, I’m chasing the perfect latte art tulip although
-          let’s be honest, it usually looks more like a confused leaf.
-          <br />
-          <br />I also dabble in digital and analog photography.
-          <br />
-          📸
-        </h3>
-      </div>
-    </section>
-  );
+	return (
+		<section className="description__section">
+			<div className="container description__container">
+				<h3>
+					Software Developer @ <a href="https://www.vier.ai/">VIER GmbH</a>
+					<br />
+				</h3>
+				<br />
+				<h3>
+					Currently diving deep into AI tools and their practical applications
+					in software development.
+					<br />
+					<br />
+					Learning more about CI/CD by building efficient pipelines for seamless code integration and delivery.
+					<br />
+					<br />
+					When I’m not coding, I’m chasing the perfect latte art tulip although
+					let’s be honest, it usually looks more like a confused leaf.
+					<br />
+					<br />I also dabble in digital and analog photography.
+					<br />
+					📸
+				</h3>
+			</div>
+		</section>
+	);
 };
 
 export default Description;
